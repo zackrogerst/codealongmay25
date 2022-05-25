ef41e8f9e6f127d6672e674cd689c094235fa36f
Zack Rogers <zack.rogers.t@gmail.com>
2022-05-25 12:30:30 -0600

    added battle for jamie attacking jon

--- codealongmay25.js
+++ codealongmay25.js
@@ -1,5 +1,16 @@
+
+
+// Jon Stats
 let jonSnowAttack = 25;
-let jamieLannisterAttack = 25;
+let jonSnowHealth = 100;
+let jonSnowDefense = 0;
+
+// Jamie Stats
+let jamieLannisterAttack = 107;
+// let jamieSnowHealth = 100;
+// let jamieSnowDefense = 0;
+
+// battle
 
 if (jonSnowAttack > jamieLannisterAttack) {
     console.log("Jon Snow has better attack than Jamie Lannister.");
@@ -9,3 +20,10 @@ if (jonSnowAttack > jamieLannisterAttack) {
     console.log("Jon Snow and Jamie Lannister have the same attack.");
 }
 
+if (jonSnowHealth <= jamieLannisterAttack) {
+    console.log("Jon Snow has been slain");
+} else {
+    // jonSnowHealth = jonSnowHealth - jamieLannisterAttack;
+    jonSnowHealth -= jamieLannisterAttack;
+    console.log(`Jon Snow's health is down to ${jonSnowHealth}`);
+}
