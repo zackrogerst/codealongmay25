d0d0f85501b1ec484c7bb7821779c000a2d599f9
Zack Rogers <zack.rogers.t@gmail.com>
2022-05-25 12:16:41 -0600

    initial commit: sizing up

---
+++ codealongmay25.js
@@ -0,0 +1,8 @@
+let jonSnowAttack = 25;
+let jamieLannisterAttack = 35;
+
+if (jonSnowAttack > jamieLannisterAttack) {
+    console.log("Jon Snow has better attack than Jamie Lannister.")
+} else {
+    console.log("Jamie Lannister has better attack than Jon Snow.")
+}
